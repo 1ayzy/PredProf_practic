5e98b57bccea8733a5df9ca604b82f459e561121
1ayzy <Westkarpov@yandex.ru>
2024-03-31 11:30:14 +0300

    working

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ url = 'https://olimp.miet.ru/ppo_it_final'
 getheads = {
     'X-Auth-Token': 'ppo_10_10320',
 }
+
 getparams = {
+    'day': '25',
+    'month': '01',
+    'year': '23',
 }
+
 response = requests.get(url=url, params=getparams, headers=getheads)
 data = response.json()
 print(data)
