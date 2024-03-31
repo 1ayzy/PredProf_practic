71432019e19323b936f0c4251e4a3537f9a95fa1
actggg <120035486+actggg@users.noreply.github.com>
2024-03-31 14:20:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
 from main import get_dates, get_date_info
-
+from database import DatabaseOperations
 app = Flask(__name__)
 
 @app.route("/")
 def dates_render():
     dates = get_dates().json()["message"]
+    DatabaseOperations().createAllDatesDatabase()
+    DatabaseOperations().addDates(dates)
     return render_template("dates.html", dates_count = len(dates), dates = dates)
 @app.route("/dates/<date>")
 def date_render(date):
     date_info = get_date_info(str(date)).json()["message"]
-    print(date_info)
-    return render_template("date.html", date_info=date_info, windows = [[[True, 1], [True, 2], [False, 1], [True, 1], [True, 1]]], date=date, numbers='1, 2' , count_numbers=2, rooms_per_floor=1, windows_per_floor=6)
+    DatabaseOperations().createDateInfoDatabase()
+    DatabaseOperations().addDateInfo(get_date_info(str(date)).json())
+    count = date_info["flats_count"]["data"]
+    windows_per_floor = date_info["windows_for_flat"]["data"]
+    #print(*windows_per_floor)
+    return render_template("date.html", date_info=date_info, windows = [[[True, 1], [True, 2]], [[False, 1], [True, 1]], [[True, 1], [False, 11]]], date=date, numbers='1, 2' , count_numbers=count, rooms_per_floor=1, windows_per_floor=" ".join(windows_per_floor))
 
 def main():
     app.run(port=5000, host='127.0.0.1', debug=True)
