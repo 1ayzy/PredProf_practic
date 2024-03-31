5e2721d2b7ed47cf338ad626fcf625185a0e3450
actggg <120035486+actggg@users.noreply.github.com>
2024-03-31 14:20:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from requests import Response
 from dataclasses import dataclass
+import unittest
 
 
 @dataclass
@@ -17,3 +18,64 @@ def get_date_info(date: str) -> Response:
     day, month, year = date.split("-")
     params = {"day": day, "month": month, "year": year}
     return requests.get(Consts.base_url, headers=Consts.headers, params=params)
+
+
+def post_check(date: str, lighted_rooms: list[list[bool]]) -> bool:
+    floors_count = len(lighted_rooms)
+    flats_on_floor = len(lighted_rooms[0])
+
+    lighted_rooms_count = 0
+    lighted_rooms_numbers = []
+    for i in range(floors_count):
+        for j in range(flats_on_floor):
+            if lighted_rooms[i][j]:
+                lighted_rooms_count += 1
+                lighted_rooms_numbers.append(i * flats_on_floor + j + 1)
+
+    data_to_check = {
+        "date": date,
+        "data": {
+            "count": lighted_rooms_count,
+            "rooms": lighted_rooms_numbers
+        }
+    }
+
+    json = requests.post(Consts.base_url, headers=Consts.headers, json=data_to_check).json()
+    return json["message"] == "correct answer"
+
+
+def get_lighted(date: str) -> (list[list[bool]], bool):
+    date_info_json = get_date_info(date).json()
+    flats_on_floor = date_info_json["message"]["flats_count"]["data"]
+    floors_count = len(date_info_json["message"]["windows"]["data"])
+    windows_for_flat = date_info_json["message"]["windows_for_flat"]["data"]
+    lighted_windows = [[] for _ in range(floors_count)]
+
+    for i in range(floors_count):
+        lighted_windows[i] = date_info_json["message"]["windows"]["data"][f"floor_{i + 1}"]
+
+    lighted_rooms = [[False for _ in range(flats_on_floor)] for _ in range(floors_count)]
+
+    for i in range(floors_count):
+        for j in range(flats_on_floor):
+            for w in range(windows_for_flat[j]):
+                count = 0
+                for n in range(j):
+                    count += windows_for_flat[n]
+
+                if lighted_windows[i][count + w]:
+                    lighted_rooms[i][j] = True
+
+    return lighted_rooms, post_check(date, lighted_rooms)
+
+
+class Tests(unittest.TestCase):
+    def test_get_lighted(self):
+        json = get_dates().json()
+        dates = json["message"]
+
+        for date in dates:
+            lighted_rooms = get_lighted(date)
+            message = post_check(date, lighted_rooms).json()["message"]
+
+            self.assertTrue(message == "correct answer", f"On date {date} lighted rooms are incorrect")
